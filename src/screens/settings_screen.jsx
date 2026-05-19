ae0919d7546eae4eba4f868216f7379794c33354
BozoBus67 <666handsomeguy999@gmail.com>
2026-05-19 17:41:56 -0400

    added feature

--- src/screens/settings_screen.jsx
+++ src/screens/settings_screen.jsx
@@ -2,6 +2,8 @@ import {
   useTheme,
   accordion_mode,
   set_accordion_mode,
+  themeName,
+  toggleTheme,
 } from '../shared/constants'
 import { Screen_Layout } from '../shared/utility_components'
 
@@ -23,6 +25,30 @@ export function Settings_Screen() {
       <p style={{ margin: 0, color: t().text }}>
         In case you're a control freak
       </p>
+      <label
+        style={{
+          display: 'flex',
+          'align-items': 'center',
+          gap: '0.6rem',
+          color: t().text_h,
+          cursor: 'pointer',
+          'margin-top': '0.5rem',
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={themeName() === 'dark'}
+          onChange={toggleTheme}
+          style={{ width: '1.1rem', height: '1.1rem', cursor: 'pointer' }}
+        />
+        <span>
+          Dark mode{' '}
+          <span style={{ color: t().text }}>
+            (this is also togglable top right corner by clicking on the
+            sun/moon)
+          </span>
+        </span>
+      </label>
       <label
         style={{
           display: 'flex',
