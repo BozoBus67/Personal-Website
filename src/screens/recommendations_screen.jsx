3c674c95715a50393b392d03605befb947679804
BozoBus67 <666handsomeguy999@gmail.com>
2026-05-20 04:09:28 -0400

    refactored

--- src/screens/recommendations_screen.jsx
+++ src/screens/recommendations_screen.jsx
@@ -19,7 +19,7 @@ export function Recommendations_Screen() {
           'font-weight': 700,
           'font-size': '48px',
           'line-height': 1,
-          margin: '2rem 0 0',
+          margin: '1rem 0 0',
           color: t().text_h,
           'text-align': 'center',
         }}
