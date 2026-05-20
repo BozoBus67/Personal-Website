a0500a35b750b2a3cc3d26773ad3155dc9ab4374
BozoBus67 <666handsomeguy999@gmail.com>
2026-05-20 14:23:02 -0400

    refactored

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -12,7 +12,7 @@ export const themes = {
   light: {
     name: 'light',
     bg: '#ffffff',
-    page_bg: 'rgba(50, 160, 240, 0.3)',
+    page_bg: 'rgba(50, 160, 240, 0.2)',
     text: '#4a4452',
     text_h: '#08060d',
     border: '#e5e4e7',
