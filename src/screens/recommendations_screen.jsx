67bb9980a4f7c0459685761afcadc89cb3179e41
BozoBus67 <666handsomeguy999@gmail.com>
2026-05-21 14:32:32 -0400

    miscellaneous dev work

--- src/screens/recommendations_screen.jsx
+++ src/screens/recommendations_screen.jsx
@@ -130,7 +130,7 @@ function Rec_Card(props) {
 
 const code_geass_blurb =
 `This is my personal favorite piece of media. The canonical anime is 2 seasons, titled "Code Geass: Lelouch of the Rebellion" and "Code Geass: Lelouch of the Rebellion R2" precisely. There are many spinoffs, ignore them. The story is fully complete. 
-If you've never watched anime before or usually watch dubbed, I highly recommend watching the subbed version.`
+If you've never watched anime before or usually watch dubbed, I highly recommend watching the subbed version.s`
 
 const transition_blurb =
 `There are others, but people's watch lists are long enough, so I would just recommend this one. But if you have the time and you're interested, here are some more:`
